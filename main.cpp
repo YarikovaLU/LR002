f37e6a2e180cd1d39af4f97604ead2521b2a4adb
Luba (YarikovaLU) <luba.rey.l@gmail.com>
2019-06-27 22:10:31 +0300

    code: добавлен вывод максимума

--- main.cpp
+++ main.cpp
@@ -10,5 +10,11 @@ int main()
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n';
+    if (a > b) {
+        cout << a << "\n";
+    }
+    else {
+        cout << b << "\n";
+    }
     return 0;
 }
